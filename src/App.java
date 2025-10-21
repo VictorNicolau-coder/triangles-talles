01c16e45379f0b940ff849e060dddf795dc9d73e
Victor Anderson Bizerra Nicolau <124687497+VictorNicolau-coder@users.noreply.github.com>
2025-10-20 21:09:23 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,12 +2,27 @@ import java.util.*;
 
 public class App {
 
+    public static boolean contains(int[] distancia_mod, int n, int p2){
+            int p = 1;
+            int r = n;
+            int q;
+            
+            while (p <= r) {
+                q = (p + r) / 2;
+                if (distancia_mod[q-1] == p2) return true;
+                else if (distancia_mod[q-1] > p2) r = q - 1;
+            else p = q + 1;
+        }
+        
+        return false;
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         
         while (in.hasNextInt()) {
             int size_array;
-            int triangleCount = 0;
+            int count = 0;
             
             //Solicitando tamanho array
             size_array = in.nextInt();
@@ -15,30 +30,41 @@ public class App {
             
             //Preenchendo array
             int i;
-            int sum_array = 0;
+            int sum = 0;
             for(i = 0; i < size_array; i++) {
                 circle[i] = in.nextInt();
-                sum_array += circle[i];
+                sum += circle[i];
             }
+
+            if (sum % 3 != 0){
+                System.out.println("0");
+                continue;
+            }
+
+            int distancia[] = new int[size_array+1];
+            for (i = 1; i <= size_array; i++)
+                distancia[i] = distancia[i-1] + circle[i-1];
 
             //Para se formar um triângulo devem ser cumpridos 3 requisitos:
             //  A soma das distâncias entre os pontos deve ser impar
             //  A quantidade de pontos deve ser maior que 2
             //  A distância entre os pontos deve ser igual
 
-            int side = sum_array/3;
-            int prefix = 0;
-            int pointCount = 0;
+            int side = sum/3;
+            int twoSide = side*2;
 
             // Percorre duas vezes para simular o array circular
-            for (i = 0; i < 2 * size_array - 1; i++) {
-                prefix += circle[i % size_array];
+            for (i = 0; i < size_array; i++) {
+                int p = distancia[i];
+                int p2 = p + side;
+                int p3 = p + twoSide;
 
-                if (prefix == 2 * side) triangleCount += pointCount;
-                if (prefix == side) pointCount++;
+                if (contains(distancia, size_array, p2) && 
+                    contains(distancia, size_array, p3))
+                    count++;
             }
 
-            System.out.println(triangleCount);
+            System.out.println(count);
         }
 
         in.close();
